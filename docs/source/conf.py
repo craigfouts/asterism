82dfd636e99ebc5f49c0c6b1f15601a82616e046
Craig <foutscw@gmail.com>
2026-09-14 16:00:37 +0100

    Updated doc requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,6 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import pydata_sphinx_theme
 import os
 import sys
 
@@ -20,7 +19,7 @@ author = 'Craig Fouts'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'pydata_sphinx_theme']
+extensions = ['sphinx.ext.autodoc']
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -28,5 +27,4 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'pydata_sphinx_theme'
-html_theme_path = pydata_sphinx_theme.get_html_theme_path()
 html_static_path = ['_static']
